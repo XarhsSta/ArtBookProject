4d15327a7c0464da2c48db4646b59d272a2f27ee
Haris Stasinos <h.stasinos72@gmail.com>
2021-07-09 23:10:01 +0300

    Update Util.kt

--- app/src/main/java/com/xarhssta/artbookproject/util/Util.kt
+++ app/src/main/java/com/xarhssta/artbookproject/util/Util.kt
@@ -2,7 +2,7 @@ package com.xarhssta.artbookproject.util
 
 object Util {
 
-    const val API_KEY = "22399501-612050e2aa13133fcf08e1efa"
+    const val API_KEY = ""
     const val BASE_URL = "https://pixabay.com"
 
 }
